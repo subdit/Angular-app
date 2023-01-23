69d978434ed50d9798d0e3cc4a8af84886d773af
Kasidit Bualoeng <subdit@gmail.com>
2023-01-23 05:43:15 -0500

    implement bootstrsp for styling and form

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Payment App';
+  name?: string;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
 }
